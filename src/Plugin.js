a8d8ed740daba1ce6d2333e203a76d255ce7ac75
AlfredMou <moujintao@outlook.com>
2018-04-17 19:02:38 +0800

    :zap: add plugin adaptation webpack 4.0

--- src/Plugin.js
+++ src/Plugin.js
@@ -10,6 +10,8 @@ const Source = require('webpack-sources');
 const utils = require('./utils');
 const PADDING = 20;
 const ReplaceSource = require('webpack-sources').ReplaceSource;
+const getAllModules = require('./getAllModules');
+const NAMESPACE = 'CssSpritePlugin';
 
 class ImageSpritePlugin {
     constructor(options) {
@@ -22,102 +24,111 @@ class ImageSpritePlugin {
         }, options);
     }
     apply(compiler) {
-        compiler.plugin('after-plugins', (compiler) => {
-            this.images = {};
-        });
+        if (compiler.hooks) {
+            compiler.hooks.afterPlugins.tap(NAMESPACE, (compiler) => this.afterPlugins());
+            compiler.hooks.thisCompilation.tap(NAMESPACE, (compilation, params) => {
+                compilation.hooks.optimizeTree.tapAsync(NAMESPACE, (chunks, modules, callback) => this.optimizeTree(callback, compilation));
+                compilation.hooks.afterOptimizeTree.tap(NAMESPACE, (modules) => this.afterOptimizeTree(compilation));
+            });
+            compiler.hooks.compilation.tap(NAMESPACE, (compilation, params) => {
+                compilation.hooks.normalModuleLoader.tap(NAMESPACE, (loaderContext) => this.normalModuleLoader(loaderContext));
+            });
+        } else {
+            compiler.plugin('after-plugins', (compiler) => this.afterPlugins());
 
-        compiler.plugin('this-compilation', (compilation, params) => {
-            const spriteImageSize = {};
-            compilation.plugin('additional-assets', (callback) => {
-                // 生成静态资源
-                let imagePaths = Object.keys(this.images);
-                imagePaths = Array.from(new Set(imagePaths)).sort();
-                const images = {};
-                for (const path of imagePaths)
-                    images[path] = this.images[path];
-                this.images = images;
-                const imageList = pickPicture(images, this.options.queryParam);
-                const task = [];
-                for (const target of Object.keys(imageList)) {
-                    const paths = imageList[target].path;
-                    const path2img = imageList[target].path2img;
-                    const imageUrl = url.resolve(compilation.options.output.publicPath || '', target + '.png');
-                    const promiseInstance = new Promise((res, rej) => {
-                        Spritesmith.run({
-                            src: paths,
-                            algorithm: 'binary-tree',
-                            padding: this.options.padding,
-                        }, (err, result) => {
-                            if (err)
-                                rej(err);
-                            res(result);
-                        });
-                    }).then((result) => {
-                        const coordinates = result.coordinates;
-                        const assets = compilation.assets;
-                        spriteImageSize[target] = result.properties;
-                        for (const path of Object.keys(coordinates)) {
-                            const image = path2img[path];
-                            const baseTarget = image.baseTarget;
-                            let baseImage;
-                            if(baseTarget)
-                                baseImage = images[baseTarget];
-                            image.message = coordinates[path];
-                            image.message.hash = utils.md5Create(result.image);
-                            image.replaceCss = this.createCss(imageUrl, image.message, result.properties, image.isRetina, baseImage);
-                        }
-                        assets[path.join(this.options.output, target + '.png')] = {
-                            source: () => result.image,
-                            size: () => result.image.length,
-                        };
-                    }).catch((err) => err);
-                    task.push(promiseInstance);
-                }
-                Promise.all(task).then((values) => {
-                    callback();
-                });
+            compiler.plugin('this-compilation', (compilation, params) => {
+                compilation.plugin('optimize-tree', (chunks, modules, callback) => this.optimizeTree(callback, compilation));
+                compilation.plugin('after-optimize-tree', (modules) => this.afterOptimizeTree(compilation));
             });
-            compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
-                const images = {};
-                for (const path of Object.keys(this.images)) {
-                    const image = this.images[path];
-                    images[image.name] = image;
-                }
-                const replaceReg = /REPLACE_BACKGROUND\([^)]*\)/g;
-                chunks.forEach((chunk) => {
-                    chunk.files.forEach((file) => {
-                        if (file.endsWith('.js') || file.endsWith('.css')) {
-                            // 处理css模块
-                            const source = compilation.assets[file];
-                            let content = compilation.assets[file].source();
-                            content = content.replace(replaceReg, (name) => images[name] ? images[name].replaceCss : name);
-                            const replaceSource = new ReplaceSource(source);
-                            replaceSource.replace(0, source.size(), content);
-                            compilation.assets[file] = replaceSource;
-                        }
-                    });
-                });
-                callback();
-            });
-        });
 
-        compiler.plugin('compilation', (compilation, params) => {
-            compilation.plugin('normal-module-loader', (loaderContext) => {
-                loaderContext.ImageSpritePlugin = this;
+            compiler.plugin('compilation', (compilation, params) => {
+                compilation.plugin('normal-module-loader', (loaderContext) => this.normalModuleLoader(loaderContext));
             });
+        }
+    }
+    afterPlugins() {
+        this.images = {};
+    }
+    optimizeTree(callback, compilation) {
+        let imagePaths = Object.keys(this.images);
+        imagePaths = Array.from(new Set(imagePaths)).sort();
+        const images = {};
+        for (const path of imagePaths)
+            images[path] = this.images[path];
+        this.images = images;
+        const imageList = pickPicture(images, this.options.queryParam);
+        const task = [];
+        for (const target of Object.keys(imageList)) {
+            const paths = imageList[target].path;
+            const path2img = imageList[target].path2img;
+            const imageUrl = url.resolve(compilation.options.output.publicPath || '', target + '.png');
+            const promiseInstance = new Promise((res, rej) => {
+                Spritesmith.run({
+                    src: paths,
+                    algorithm: 'binary-tree',
+                    padding: this.options.padding,
+                }, (err, result) => {
+                    if (err)
+                        rej(err);
+                    res(result);
+                });
+            }).then((result) => {
+                const coordinates = result.coordinates;
+                const assets = compilation.assets;
+                for (const path of Object.keys(coordinates)) {
+                    const image = path2img[path];
+                    const baseTarget = image.baseTarget;
+                    let baseImage;
+                    if (baseTarget)
+                        baseImage = images[baseTarget];
+                    image.message = coordinates[path];
+                    image.message.hash = utils.md5Create(result.image);
+                    image.replaceCss = this.createCss(imageUrl, image.message, result.properties, image.isRetina, baseImage);
+                }
+                assets[path.join(this.options.output, target + '.png')] = {
+                    source: () => result.image,
+                    size: () => result.image.length,
+                };
+            }).catch((err) => err);
+            task.push(promiseInstance);
+        }
+        Promise.all(task).then((values) => {
+            callback();
         });
+    }
+    afterOptimizeTree(compilation) {
+        const allModules = getAllModules(compilation);
+        const images = {};
+        const replaceReg = /REPLACE_BACKGROUND\([^)]*\)/g;
+        for (const path of Object.keys(this.images)) {
+            const image = this.images[path];
+            images[image.name] = image;
+        }
+        allModules.forEach((module) => {
+            const source = module._source;
+            if (typeof source === 'string') {
+                module._source = this.replaceHolder(module._source, replaceReg, images);
+            } else if (typeof source === 'object' && typeof source._value === 'string') {
+                source._value = this.replaceHolder(source._value, replaceReg, images);
+            }
+        });
+    }
+    normalModuleLoader(loaderContext) {
+        loaderContext.ImageSpritePlugin = this;
+    }
+    replaceHolder(value, replaceReg, images) {
+        return value.replace(replaceReg, (name) => images[name] ? images[name].replaceCss : name);
     }
     createCss(imageUrl, message, properties, isRetina, baseImage) {
         const { x, y, hash } = message;
         const { width, height } = properties;
-        console.log();
-        if(isRetina){
+        if (isRetina) {
             const baseWidth = baseImage.size.width;
             const baseHeight = baseImage.size.height;
             const proportionWidth = baseWidth / width;
             const proportionHeight = baseHeight / height;
-            return `url(${imageUrl}?${hash}) no-repeat;background-position: -${x*proportionHeight}px -${y*proportionHeight}px;background-size:${width*proportionWidth}px ${height*proportionHeight}px;`;
-        }else
+            return `url(${imageUrl}?${hash}) no-repeat;background-position: -${x * proportionHeight}px -${y * proportionHeight}px;background-size:${width * proportionWidth}px ${height * proportionHeight}px;`;
+        } else
             return `url(${imageUrl}?${hash}) no-repeat;background-position: -${x}px -${y}px`;
     }
 }
